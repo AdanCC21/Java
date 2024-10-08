4d9f5a9b0071bc2392959c16234c9ce0a1a8809b
AdanCC21 <141715477+AdanCC21@users.noreply.github.com>
2024-10-07 22:10:52 -0700

    hashMap added

--- Practica1/src/Bank.java
+++ Practica1/src/Bank.java
@@ -1,10 +1,14 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Bank {
     String name;
     List<AccuntHolder> ah_cont = new ArrayList<>();
     List<Account> acc_cont = new ArrayList<>();
+
+    HashMap <Integer, AccuntHolder> ahCont = new HashMap<>();
+    HashMap <Integer, Account> acCont = new HashMap<>();
 
     public Bank (){
         this.name = "CimaBank";
@@ -13,30 +17,46 @@ public class Bank {
 
     public void createAccountHolder(int id, String name, String curp){
         AccuntHolder tempAch = new AccuntHolder(id,name,curp);
-        ah_cont.add(tempAch);
+//        ah_cont.add(tempAch);
+        ahCont.put(id,tempAch);
         createAccount(id,0);
     }
 
     public void createAccount(int id, int balance){
         Account tempAccount = new CheckingAccount(id,balance);
-        acc_cont.add(tempAccount);
+//        acc_cont.add(tempAccount);
+        acCont.put(id,tempAccount);
 
     }
 
     public Account getAccount(int id){
-        for(int i =0;i<= acc_cont.size();i++){
-            Account temp = acc_cont.get(i);
+//        for(int i =0;i<= acCont.size();i++){
+//            Account temp = acCont.get(i);
+//            if(temp != null){
+//                if(temp.id == id){
+//                    return temp ;
+//                }
+//            }
+//        }
+//        return  null;
+        for(Account temp : acCont.values()){
             if(temp.id == id){
-                return temp ;
+                return temp;
             }
         }
-        return  null;
+        return null;
     }
 
     public AccuntHolder getAccountHolder(int id){
-        for(int i =0; i<= acc_cont.size();i++){
-            AccuntHolder temp = ah_cont.get(i);
-
+//        for(int i =0; i<= acc_cont.size();i++){
+//            AccuntHolder temp = ah_cont.get(i);
+//
+//            if(temp.id == id){
+//                return temp;
+//            }
+//        }
+//        return  null;
+        for(AccuntHolder temp : ahCont.values()){
             if(temp.id == id){
                 return temp;
             }
@@ -47,10 +67,14 @@ public class Bank {
     public void printReport(){
         System.out.println("These are all the transactions ");
 
-        for(int i =0;i < acc_cont.size() ; i++){
-            Account temp = acc_cont.get(i);
+        for(Account temp : acCont.values()){
             System.out.println("id : "+temp.id+" Balance : "+temp.balance+" Last transaction : "+temp.date_of_last_transaction);
         }
+
+//        for(int i =0;i < acc_cont.size() ; i++){
+//            Account temp = acCont.get(i);
+//            System.out.println("id : "+temp.id+" Balance : "+temp.balance+" Last transaction : "+temp.date_of_last_transaction);
+//        }
     }
 
     public void deposit(int source_id,int amount){
